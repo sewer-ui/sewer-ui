69ca00df66abe86b3d7ff87b6c68cd2facc50fba
jc49520 <>
2019-06-14 15:31:22 +0800

    1

---
+++ src/contract/components/foot.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="footer">
+      <div class="content">
+
+      </div>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+    },
+    created(){},
+       
+    }
+</script>
+
+<style lang="css" scope>
+.footer{
+    width: 100%;
+    color: #a3a4a5;
+    font-size: 14px;
+    background-color: #3d4450;
+}
+.content{
+    min-width: 1300px;
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+</style>
