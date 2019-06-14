34a574a6a1ad544d785e6e57a496f1e875e72fdd
jc49520 <>
2019-06-14 16:17:49 +0800

    1

--- src/contract/router/index.js
+++ src/contract/router/index.js
@@ -1,8 +1,14 @@
 import adminLogin from "../views/adminLogin.vue"
+import myabout from "../views/myabout.vue"
+
 
 const contractRouter = [{
     path: "/adminLogin",
     name: "adminLogin",
     component: adminLogin
+}, {
+    path: "/myabout",
+    name: "myabout",
+    component: myabout
 }]
 export default contractRouter
---
+++ src/contract/views/myabout.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    1
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+     
+    };
+  },
+  created() {},
+};
+</script>
+
+<style lang="less" scope>
+.bglogin {
+  width: 100%;
+  background: url("../../../static/img/bg1.png") no-repeat 50%;
+  min-width: 1300px;
+  height: 755px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  .logBox {
+    padding: 30px;
+  }
+  .logTit {
+    font-family: Microsoft YaHei UI;
+    font-weight: 400;
+    font-size: 22px;
+    height: 60px;
+    line-height: 60px;
+    text-align: center;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    color: #6e6e6e;
+    background: #e3e3e3;
+  }
+
+  .imgBox {
+    width: 574px;
+    height: 304px;
+    margin-right: 100px;
+  }
+  .imgBox img {
+    width: 100%;
+  }
+  .login {
+    width: 360px;
+    border-radius: 5px;
+    background: #fff;
+  }
+  .btn {
+    background-color: #0b8aee;
+    border: none;
+    height: 36px;
+    margin-bottom: 20px;
+  }
+  .btn:hover {
+    background-color: #0b8aee;
+    border: none;
+  }
+}
+</style>
