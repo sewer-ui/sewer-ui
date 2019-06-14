f9c67622f96c7066890b69c974d4718f9fecd6fc
xuwei <xw49546@ly.com>
2019-06-14 16:06:50 +0800

    tab样式完成

--- src/contract/components/nav.vue
+++ src/contract/components/nav.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="nav-wrap">
-      <div class="left-box">
-        <div class="logo">
-          <span>TBaaS</span>
-          <span>开放服务平台</span>
-        </div>
-        <div class="left-nav"></div>
+    <div class="left-box">
+      <div class="logo">
+        <span class="logo-name">TCBaaS</span>
+        <span>开放服务平台</span>
       </div>
-      <div class="right-box"></div>
+      <div class="left-nav" @click="clickNav">
+        <span v-for="(item,index) in leftNav" :key="index" :class="{'active-color':activeIndex===index}" :id="index">{{item.tit}}</span>
+      </div>
+    </div>
+    <div class="right-box">
+         <span v-for="(item,index) in rightNav" :key="index">{{item.tit}}</span>
+         <span class="login-btn active-color">登录</span>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
+        activeIndex:0,
       leftNav: [
         { tit: "首页", path: "" },
         { tit: "文档", path: "" },
@@ -21,21 +27,68 @@ export default {
       ],
       rightNav: [{ tit: "控制台", path: "" }]
     };
+  },
+  created(){
+
+  },
+  methods:{
+      clickNav(e){
+          
+          if( e.target.id==='') return;
+          let index = e.target.id*1
+          console.log("e",index)
+          this.activeIndex = index
+      }
   }
 };
 </script>
 <style lang="less" scoped>
+ .active-color{
+            color: #0b8aee!important;
+        }
 .nav-wrap {
   min-width: 1300px;
   height: 73px;
   background-color: #2b303b;
   z-index: 100000;
-    color: #999;
-  .nav-box {
-    height: 45px;
-    min-width: 1250px;
-    padding: 13px 0;
-    margin: 0 auto;
+  color: #999;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10%;
+  box-sizing: border-box;
+  .left-box {
+    font-size: 16px;
+    display: flex;
+    justify-content:start;
+    align-items: center;
+    .logo {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      font-size: 12px;
+      .logo-name {
+        font-size: 20px;
+        color: #fff;
+      }
+    }
+    .left-nav{
+        margin:0 40px ;
+        span{
+            margin-right: 45px;
+        }
+       
+    }
+  }
+  .right-box{
+      span{
+          color: #fff;
+          font-size: 16px;
+      }
+      .login-btn{
+          margin-left: 20px;
+      }
   }
 }
 </style>
